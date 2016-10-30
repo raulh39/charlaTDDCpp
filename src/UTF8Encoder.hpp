373147f4bbbeaf9032192d7cd020b7ce2abfcd22
Raul Huertas <raulh39@gmail.com>
2016-10-30 09:24:26 +0000

    Ending first phase of UTF8Encoder

---
+++ src/UTF8Encoder.hpp
@@ -0,0 +1,42 @@
+#ifndef UTF8_ENCODER_HPP_GUARD
+#define UTF8_ENCODER_HPP_GUARD
+
+class UTF8Encoder
+{
+	public:
+		static constexpr char32_t lower_6_bits   = 0b0011'1111UL;
+		static constexpr char32_t continuation_byte = 0b1000'0000UL;
+		static constexpr char32_t two_bytes_preamble   = 0b1100'0000UL;
+		static constexpr char32_t three_bytes_preamble = 0b1110'0000UL;
+		static constexpr char32_t four_bytes_preamble  = 0b1111'0000UL;
+
+		std::string encode(char32_t c) {
+			if(c<0x80)
+				return std::string{static_cast<char>(c)};
+			if(c<0x0800) {
+				std::string to_return{0,2};
+				to_return[1] = (c     & lower_6_bits) + continuation_byte;
+				to_return[0] = (c>>6) + two_bytes_preamble;
+				return to_return;
+			}
+			if(c<0x10000) {
+				std::string to_return{0,3};
+				to_return[2] = (c     & lower_6_bits) + continuation_byte;
+				to_return[1] = (c>>6  & lower_6_bits) + continuation_byte;
+				to_return[0] = (c>>12) + three_bytes_preamble;
+				return to_return;
+			}
+			if(c<0x110000) {
+				std::string to_return{0,4};
+				to_return[3] = (c     & lower_6_bits) + continuation_byte;
+				to_return[2] = (c>>6  & lower_6_bits) + continuation_byte;
+				to_return[1] = (c>>12 & lower_6_bits) + continuation_byte;
+				to_return[0] = (c>>18 & lower_6_bits) + four_bytes_preamble;
+				return to_return;
+			}
+			return "";
+		}
+
+};
+
+#endif //UTF8_ENCODER_HPP_GUARD
--- tdd/utf8encoder.cpp
+++ tdd/utf8encoder.cpp
@@ -1,35 +1,80 @@
 #include "catch.hpp"
-
-class UTF8Encoder
-{
-  public:
-  	 std::string encode(char32_t c) {
-  	 	if(c<0x7f)
-  	 		return std::string{static_cast<char>(c)};
-  	 	std::string to_return{0,2};
-  	 	to_return[0] = (c>>6 & 0b0001'1111) + 0b1100'0000;
-  	 	to_return[1] = (c    & 0b0011'1111) + 0b1000'0000;
-  	 	return to_return;
-  	 }
-};
+#include "UTF8Encoder.hpp"
 
 SCENARIO("UTF8Encoder its used to encode utf8", "") {
 	GIVEN("An instance of UTF8Encoder") {
 		UTF8Encoder utf8enc;
 
-		WHEN("Encoding an 'a'") {
+		WHEN("Encoding an 'a' (0x61) unicode code point") {
 			auto encoded = utf8enc.encode(0x61);
 			THEN("You get an 'a'") {
 				REQUIRE(encoded[0] == 0x61);
 			}
 		}
 
-		WHEN("Encoding an 'ñ'") {
+		WHEN("Encoding a DELETE (7f) unicode code point") {
+			auto encoded = utf8enc.encode(0x7f);
+			THEN("You get the same 7f") {
+				REQUIRE(encoded[0] == 0x7f);
+			}
+		}
+
+		WHEN("Encoding a 'ñ' (0xf1) unicode code point") {
 			auto encoded = utf8enc.encode(0xf1);
 			THEN("You get an 0xc3 0xb1") {
 				REQUIRE(encoded[0] == '\xc3');
 				REQUIRE(encoded[1] == '\xb1');
 			}
 		}
+
+		WHEN("Encoding the bigger two byte number") {
+			auto encoded = utf8enc.encode(0x07ff);
+			THEN("You get an 0xDF 0xBF") {
+				REQUIRE(encoded[0] == '\xdf');
+				REQUIRE(encoded[1] == '\xbf');
+			}
+		}
+
+		WHEN("Encoding a 0x0DA3 unicode code point") {
+			auto encoded = utf8enc.encode(0x0DA3);
+			THEN("You get 0xE0 0xB6 0xA3") {
+				REQUIRE(encoded[0] == '\xe0');
+				REQUIRE(encoded[1] == '\xb6');
+				REQUIRE(encoded[2] == '\xa3');
+			}
+		}
+
+		WHEN("Encoding the bigger three byte number") {
+			auto encoded = utf8enc.encode(0x0ffff);
+			THEN("You get an EF BF BF") {
+				REQUIRE(encoded[0] == '\xef');
+				REQUIRE(encoded[1] == '\xbf');
+				REQUIRE(encoded[2] == '\xbf');
+			}
+		}
+
+		WHEN("Encoding a 0x10082") {
+			auto encoded = utf8enc.encode( 0x10082 );
+			THEN("You get 0xF0 0x90 0x82 0x82") {
+				REQUIRE(encoded[0] == '\xf0');
+				REQUIRE(encoded[1] == '\x90');
+				REQUIRE(encoded[2] == '\x82');
+				REQUIRE(encoded[3] == '\x82');
+			}
+		}
+
+		WHEN("Encoding biggest Unicode code point") {
+			auto encoded = utf8enc.encode( 0x10ffff );
+			THEN("You get F4 8F BF BF") {
+				REQUIRE(encoded[0] == '\xf4');
+				REQUIRE(encoded[1] == '\x8f');
+				REQUIRE(encoded[2] == '\xbf');
+				REQUIRE(encoded[3] == '\xbf');
+			}
+		}
 	}
 }
+
+/*
+TODO:
+*/
